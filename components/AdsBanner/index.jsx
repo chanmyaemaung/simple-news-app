2e69fb7e89a35591afd9cfe748269e9f2fa56996
Chen Lay <maungmaungyoker.mmy@gmail.com>
2022-01-05 23:12:38 +0630

    Update Sponsored Image and Link :pushpin:

--- components/AdsBanner/index.jsx
+++ components/AdsBanner/index.jsx
@@ -2,19 +2,26 @@ import { Box } from '@mui/material'
 import Image from 'next/image'
 
 export default function AdsBanner() {
+	const appLink =
+		'https://play.google.com/store/apps/details?id=com.pdfsupporter'
+
+	const sponsorImage = 'https://i.imgur.com/yWkVc8X.gif'
+
 	return (
 		<Box sx={{ width: '100%', cursor: 'pointer', p: 1, mr: -1.3 }}>
-			<Image
-				src='https://via.placeholder.com/720x90'
-				alt='Ads banner'
-				width={720}
-				height={90}
-				objectFit='cover'
-				objectPosition='center'
-				quality={100}
-				layout='responsive'
-				priority
-			/>
+			<a href={appLink} target='_blank'>
+				<Image
+					src={sponsorImage}
+					alt='Ads banner'
+					width={720}
+					height={90}
+					objectFit='cover'
+					objectPosition='center'
+					quality={100}
+					layout='responsive'
+					priority
+				/>
+			</a>
 		</Box>
 	)
 }
